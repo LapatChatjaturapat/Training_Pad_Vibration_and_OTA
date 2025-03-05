d701c83b12fdba34799bd8952d016765ccea1545
LCM <Lapatchat@GMAIL.COM>
2025-03-05 15:08:09 +0700

    initial commit

---
+++ OTA/Training_Pad_OTA_Firmware.c
@@ -0,0 +1,196 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/gpio.h"
+#include "driver/ledc.h"
+#include "esp_err.h"
+#include "esp_log.h"
+#include "driver/rmt.h"
+#include "led_strip.h"
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
+#include "freertos/semphr.h"
+#include <stdlib.h>
+#include <sys/time.h>
+#include "esp_sleep.h"
+#include "driver/rtc_io.h"
+
+#include <esp_http_server.h>
+#include <esp_ota_ops.h>
+#include <esp_system.h>
+#include <nvs_flash.h>
+#include <sys/param.h>
+#include <esp_wifi.h>
+
+#define WIFI_SSID "Triaing_Pad OTA Update"
+
+static bool is_ota_mode = false;
+extern const uint8_t index_html_start[] asm("_binary_index_html_start");
+extern const uint8_t index_html_end[] asm("_binary_index_html_end");
+
+esp_err_t index_get_handler(httpd_req_t *req)
+{
+	httpd_resp_send(req, (const char *) index_html_start, index_html_end - index_html_start);
+	return ESP_OK;
+}
+
+/*
+ * Handle OTA file upload
+ */
+esp_err_t update_post_handler(httpd_req_t *req)
+{
+	char buf[1000];
+	esp_ota_handle_t ota_handle;
+	int remaining = req->content_len;
+
+	const esp_partition_t *ota_partition = esp_ota_get_next_update_partition(NULL);
+	ESP_ERROR_CHECK(esp_ota_begin(ota_partition, OTA_SIZE_UNKNOWN, &ota_handle));
+
+	while (remaining > 0) {
+		int recv_len = httpd_req_recv(req, buf, MIN(remaining, sizeof(buf)));
+
+		// Timeout Error: Just retry
+		if (recv_len == HTTPD_SOCK_ERR_TIMEOUT) {
+			continue;
+
+		// Serious Error: Abort OTA
+		} else if (recv_len <= 0) {
+			httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Protocol Error");
+			return ESP_FAIL;
+		}
+
+		// Successful Upload: Flash firmware chunk
+		if (esp_ota_write(ota_handle, (const void *)buf, recv_len) != ESP_OK) {
+			httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Flash Error");
+			return ESP_FAIL;
+		}
+
+		remaining -= recv_len;
+	}
+
+	// Validate and switch to new OTA image and reboot
+	if (esp_ota_end(ota_handle) != ESP_OK || esp_ota_set_boot_partition(ota_partition) != ESP_OK) {
+			httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Validation / Activation Error");
+			return ESP_FAIL;
+	}
+
+	httpd_resp_sendstr(req, "Firmware update complete, rebooting now!\n");
+
+	vTaskDelay(500 / portTICK_PERIOD_MS);
+	esp_restart();
+
+	return ESP_OK;
+}
+
+/*
+ * HTTP Server
+ */
+httpd_uri_t index_get = {
+	.uri	  = "/",
+	.method   = HTTP_GET,
+	.handler  = index_get_handler,
+	.user_ctx = NULL
+};
+
+httpd_uri_t update_post = {
+	.uri	  = "/update",
+	.method   = HTTP_POST,
+	.handler  = update_post_handler,
+	.user_ctx = NULL
+};
+
+static esp_err_t http_server_init(void)
+{
+	static httpd_handle_t http_server = NULL;
+
+	httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+
+	if (httpd_start(&http_server, &config) == ESP_OK) {
+		httpd_register_uri_handler(http_server, &index_get);
+		httpd_register_uri_handler(http_server, &update_post);
+	}
+
+	return http_server == NULL ? ESP_FAIL : ESP_OK;
+}
+
+/*
+ * WiFi configuration
+ */
+static esp_err_t softap_init(void)
+{
+	esp_err_t res = ESP_OK;
+
+	res |= esp_netif_init();
+	res |= esp_event_loop_create_default();
+	esp_netif_create_default_wifi_ap();
+
+	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+	res |= esp_wifi_init(&cfg);
+
+	wifi_config_t wifi_config = {
+		.ap = {
+			.ssid = WIFI_SSID,
+			.ssid_len = strlen(WIFI_SSID),
+			.channel = 6,
+			.authmode = WIFI_AUTH_OPEN,
+			.max_connection = 3
+		},
+	};
+
+	res |= esp_wifi_set_mode(WIFI_MODE_AP);
+	res |= esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config);
+	res |= esp_wifi_start();
+
+	return res;
+}
+
+void start_ota_mode() {
+    is_ota_mode = true;  // set flag ว่าอยู่ใน OTA mode
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+    // Stop BLE 
+    esp_bluedroid_disable();
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+    esp_bluedroid_deinit();
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+    esp_bt_controller_disable();
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+    esp_bt_controller_deinit();
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+    esp_bt_controller_mem_release(ESP_BT_MODE_BLE);
+    // Start OTA
+    ESP_ERROR_CHECK(softap_init());
+    ESP_ERROR_CHECK(http_server_init());
+    
+    printf("OTA Update Mode Started\n");
+    while(1) {
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+
+}
+
+void app_main(void)
+{
+    ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK( ret ); 
+    
+    const esp_partition_t *partition = esp_ota_get_running_partition();
+	printf("Currently running partition: %s\r\n", partition->label);
+
+	esp_ota_img_states_t ota_state;
+	if (esp_ota_get_state_partition(partition, &ota_state) == ESP_OK) {
+		if (ota_state == ESP_OTA_IMG_PENDING_VERIFY) {
+			esp_ota_mark_app_valid_cancel_rollback();
+		}
+	}
+    while(1) {
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
